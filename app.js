62ab82474443ac1e0fa8b96ccf146e8067454d1f
addaskogberg <adda.skogberg@gmail.com>
2018-02-13 19:58:39 +0100

    handlebar renders

--- app.js
+++ app.js
@@ -2,40 +2,38 @@
 var express = require('express')
 
 var app = express()
+app.set('port', process.env.PORT || 3000)
 
-// set upp handlebars view engine
+ // set upp handlebars view engine
 var handlebars = require('express3-handlebars')
-  .create({defaultLayout:'main' })
+  .create({ defaultLayout: 'main' })
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
-
-app.set('port', process.env.PORT || 3000)
-
-app.get('/', function(req, res){
-  res.render('home')
+app.get('/', function (req, res) {
+  res.render('layouts/home')
 })
-app.get('/form', function(req, res){
-  res.render('form')
+app.get('/form', function (req, res) {
+  res.render('layouts/form')
 })
 
 // 404 page
-app.use(function(err, req, res, next){
+app.use(function (req, res, next) {
   res.status(404)
-  res.send('404 - not found')
+  res.render('error/404')
 })
 // 400 page
-app.use(function(err, req, res, next){
+app.use(function (req, res, next) {
   res.status(400)
-  res.send('400 - not found')
+  res.render('error/400')
 })
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500)
-  res.send('500 - server error')
+  res.render('error/500')
 })
 
-app.listen(app.get('port'), function(){
-  console.lognp('Express started on http://localhost:' + app.get('port')+ ' ; press ctrl-c to terminate')
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') + ' ; press ctrl-c to terminate')
 })
