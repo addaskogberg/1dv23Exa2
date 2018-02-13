fbf815c28286c49aa1d5dc36cbbb7de8a6a907ad
addaskogberg <adda.skogberg@gmail.com>
2018-02-13 13:22:08 +0100

    routes for handlebars

--- app.js
+++ app.js
@@ -12,16 +12,26 @@ app.set('view engine', 'handlebars')
 
 app.set('port', process.env.PORT || 3000)
 
+app.get('/', function(req, res){
+  res.render('home')
+})
+app.get('/form', function(req, res){
+  res.render('form')
+})
+
 // 404 page
-app.use(function(req, res){
-  res.type('text/plain')
+app.use(function(err, req, res, next){
   res.status(404)
   res.send('404 - not found')
+})
+// 400 page
+app.use(function(err, req, res, next){
+  res.status(400)
+  res.send('400 - not found')
 })
 
 app.use(function(err, req, res, next){
   console.error(err.stack)
-  res.type('text/plain')
   res.status(500)
   res.send('500 - server error')
 })
